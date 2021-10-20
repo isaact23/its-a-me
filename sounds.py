04a3d78d44c7b42420534305d5262b1ba0ce5c76
shufflesninja <isaact23@vt.edu>
2021-10-20 13:17:29 -0400

    Convert sounds to OGG to reduce space (300 MB reduced to 17 MB)

--- sounds.py
+++ sounds.py
@@ -1,13 +1,14 @@
+import random
 from pygame import mixer
 
 mixer.init()
 mixer.music.set_volume(1)
 
-UNDERTALE = mixer.Sound("sounds/undertale.wav")
+UNDERTALE = mixer.Sound("sounds/Undertale.ogg")
 
-SHATTER_1 = mixer.Sound("sounds/Glass Shatter 1.wav")
-SHATTER_2 = mixer.Sound("sounds/Glass Shatter 2.wav")
-SHATTER_3 = mixer.Sound("sounds/Glass Shatter 3.wav")
+SHATTER_1 = mixer.Sound("sounds/Glass Shatter 1.ogg")
+SHATTER_2 = mixer.Sound("sounds/Glass Shatter 2.ogg")
+SHATTER_3 = mixer.Sound("sounds/Glass Shatter 3.ogg")
 
 FINAL_FANTASY = mixer.Sound("sounds/Final Fantasy VII - One Winged Angel.ogg")
 
@@ -21,6 +22,12 @@ class SoundPlayer:
         :param sound: The Sound object to play.
         """
         sound.play()
+
+    def shatter(self):
+        """
+        Play a random glass shattering sound.
+        """
+        self.play(random.choice([SHATTER_1, SHATTER_2, SHATTER_3]))
 
     def stop(self):
         mixer.stop()
